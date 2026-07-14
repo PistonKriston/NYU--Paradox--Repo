13a0aaff6c046d5dd05f5ec53da53cc88d596deb
PistonKriston <nateohare10@gmail.com>
2026-07-14 15:42:15 -0400

    Level/Enemy changes

---
+++ C4GD Final/Assets/Scripts/EnemyTurnAround.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurnAround : MonoBehaviour
+{
+    public Transform left_check_point;
+    public Transform right_check_point;
+    public LayerMask BothLayer;
+    public float groundCheckRadius = 0.01f;
+
+    public bool left_side;
+
+
+    public GameObject Enemy;
+
+    public float flip_cooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        left_side = CheckLeft();
+
+        if (flip_cooldown != 0)
+        {
+            flip_cooldown-= Time.deltaTime;
+        }
+        else
+        {
+            flip_cooldown = 0;
+            if (left_side != true)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+                Enemy.GetComponent<Enemy>().facing *= -1;
+                Debug.Log("The Enemy should have flipped");
+                flip_cooldown = 0.1f;
+            }
+        }
+        
+
+    }
+
+    
+    bool CheckLeft()
+    {
+        if (Physics2D.OverlapCircle(left_check_point.position, groundCheckRadius, BothLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    
+}
