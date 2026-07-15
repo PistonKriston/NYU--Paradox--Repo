9149993b0e102615f0e6ba5335b40ce37acee538
noam-nyu <noambenshai@icloud.com>
2026-07-15 14:22:34 -0500

    Sword animation + hollowknight attacks

--- C4GD Final/Assets/Scripts/Combat.cs
+++ C4GD Final/Assets/Scripts/Combat.cs
@@ -19,26 +19,34 @@ public class Combat : MonoBehaviour
         if (Input.GetKey("w") && Input.GetMouseButtonDown(0))
             {
                 print("Help");
-                playerPosition = new Vector2(player.transform.position.x + 1, player.transform.position.y);
-                GameObject s = Instantiate(sword, player.transform);
-               s.transform.localPosition = new Vector3(0,2,0);
+                playerPosition = new Vector2(player.transform.position.x, player.transform.position.y + 1.5f);
+                print(playerPosition);
+                GameObject s = Instantiate(sword, playerPosition, Quaternion.Euler(0f, 0f, 90f));
+                //s.transform.position.x = player.transform.position.x;
+               //s.transform.localPosition = new Vector3(0,2,0);
+               s.GetComponent<Sword>().direction = new Vector2(0,1);
+               s.GetComponent<Sword>().player = gameObject;
                s.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
                GameObject.Destroy(s,0.325f);
             }
         else if (Input.GetMouseButtonDown(0))
         {
             playerPosition = new Vector2(player.transform.position.x + 1, player.transform.position.y);
-            GameObject s = Instantiate(sword, player.transform);
+            GameObject s = Instantiate(sword, playerPosition, transform.rotation);
             
             if (PlayerController.instance.facingRight == true)
             {
-                s.transform.localPosition = new Vector3(1, 0, 0);
+                //s.transform.localPosition = new Vector3(1, 0, 0);
                 //StartCoroutine(Wait(s,0.325f));
-                
+                s.GetComponent<Sword>().direction = new Vector2(1,0);
+                 s.GetComponent<Sword>().player = gameObject;
             }
             if (PlayerController.instance.facingRight == false)
             {
-                s.transform.localPosition = new Vector3(1, 0, 0);
+                //s.transform.localPosition = new Vector3(1, 0, 0);
+                s.GetComponent<Sword>().direction = new Vector2(-1,0);
+                 s.GetComponent<Sword>().player = gameObject;
+                 s.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
             }
             
             GameObject.Destroy(s,0.325f);
