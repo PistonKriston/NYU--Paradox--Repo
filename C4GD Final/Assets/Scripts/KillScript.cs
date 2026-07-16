720d21bb8886636cda7672d4202aeb510b79698f
PistonKriston <nateohare10@gmail.com>
2026-07-16 11:23:39 -0400

    Section_3, LavaDamage script, and slight change to Section_0 and the Player prefab's tag

--- C4GD Final/Assets/Scripts/KillScript.cs
+++ C4GD Final/Assets/Scripts/KillScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class KillScript : MonoBehaviour
 {
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,11 @@ public class KillScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
